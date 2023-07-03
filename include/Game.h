88d812bf18de587a6f757dbdd31b1a29a5c74338
Dasuxd <aihidao@126.com>
2023-07-03 22:11:05 +0800

    src include update

---
+++ include/Game.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <SDL.h>
+#include <SDL_image.h>
+
+static int SCREEN_WIDTH = 800;
+static int SCREEN_HEIGHT = 800;
+class Game {
+private:
+    //The window we'll be rendering to
+    SDL_Window* mWindow = NULL;
+
+    //The window renderer
+    SDL_Renderer* mRenderer = NULL;
+
+public:
+
+    bool start();
+    //Starts up SDL and creates window
+    bool init();
+
+    //Loads media
+    bool loadMedias();
+
+
+    void update();
+
+    void calculateFps();
+
+
+    void updateDraw();
+
+    //刷新事件函数
+    Uint32 timerCallBack();
+
+    //Frees media and shuts down SDL
+    void close();
+};
---
+++ src/Game.cpp
@@ -0,0 +1,183 @@
+/*This source code copyrighted by Lazy Foo' Productions 2004-2023
+and may not be redistributed without written permission.*/
+
+//Using SDL, SDL_image, standard IO, and strings
+#include "Game.h"
+#include <SDL_ttf.h>
+#include <iostream>
+#include <stdio.h>
+#include <string>
+
+void Game::update() {
+	//Main loop flag
+	bool quit = false;
+
+
+
+	//Event handler
+	SDL_Event e;
+
+	//While application is running
+	while (!quit)
+	{
+		//Handle events on queue
+		while (SDL_PollEvent(&e) != 0)
+		{
+			//User requests quit
+			if (e.type == SDL_QUIT) {
+				quit = true;
+			}
+			else if (e.type == SDL_USEREVENT) {
+				//updateDraw();
+			}
+			else if (e.type == SDL_MOUSEMOTION)
+			{
+
+			}
+			else if (e.type == SDL_MOUSEBUTTONDOWN) {
+				
+			}
+			else if (e.type == SDL_MOUSEBUTTONUP) {
+
+			}
+		}
+		calculateFps();
+		updateDraw();
+	}
+}
+
+void Game::updateDraw() {
+	SDL_SetRenderDrawColor(mRenderer, 0x00, 0x00, 0x00, 0xFF);
+	//Clear screen
+	SDL_RenderClear(mRenderer);
+
+	//Update screen
+	SDL_RenderPresent(mRenderer);
+
+}
+
+void Game::calculateFps() {
+	//frameCount++;
+}
+
+//void Game::setTimmers() {
+//	mDrawTimer = SDL_AddTimer(16, TimeEvent::drawCallback, NULL);
+//}
+
+bool Game::start() {
+	//Start up SDL and create window
+	if (!init())
+	{
+		printf("Failed to initialize!\n");
+	}
+	else
+	{
+		//Load media
+		if (!loadMedias())
+		{
+			printf("Failed to load media!\n");
+		}
+		else
+		{
+			//timmers
+			update();
+		}
+	}
+
+	//Free resources and close SDL
+	close();
+	return true;
+}
+
+
+bool Game::init()
+{
+	//Initialization flag
+	bool success = true;
+
+	//Initialize SDL
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		printf("SDL could not initialize! SDL Error: %s\n", SDL_GetError());
+		success = false;
+	}
+	else
+	{
+		//Set texture filtering to linear
+		if (!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1"))
+		{
+			printf("Warning: Linear texture filtering not enabled!");
+		}
+
+		//Create window
+		this->mWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		if (mWindow == NULL)
+		{
+			printf("Window could not be created! SDL Error: %s\n", SDL_GetError());
+			success = false;
+		}
+		else
+		{
+			//Create renderer for window
+			mRenderer = SDL_CreateRenderer(mWindow, -1, SDL_RENDERER_ACCELERATED);
+			if (mRenderer == NULL)
+			{
+				printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
+				success = false;
+			}
+			else
+			{
+
+				//Initialize renderer color
+				SDL_SetRenderDrawColor(mRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+
+				//Initialize PNG loading
+				int imgFlags = IMG_INIT_PNG;
+				if (!(IMG_Init(imgFlags) & imgFlags))
+				{
+					printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
+					success = false;
+				}
+
+				//Initialize SDL_ttf
+				if (TTF_Init() == -1)
+				{
+					printf("SDL_ttf could not initialize! SDL_ttf Error: %s\n", TTF_GetError());
+					success = false;
+				}
+			}
+		}
+	}
+
+	return success;
+}
+
+bool Game::loadMedias()
+{
+	//Loading success flag
+	bool success = true;
+
+	//mGameTexture = new GameTexture(mRenderer, "res/test.png");
+	//if (mGameTexture == NULL)
+	//{
+	//	printf("Failed to load texture res/test.png!\n");
+	//	success = false;
+	//}
+
+	return success;
+}
+
+void Game::close()
+{
+	//Destroy window	
+	SDL_DestroyRenderer(mRenderer);
+	SDL_DestroyWindow(mWindow);
+	mWindow = NULL;
+	mRenderer = NULL;
+
+	//Quit SDL subsystems
+	TTF_Quit();
+	IMG_Quit();
+	SDL_Quit();
+}
+
